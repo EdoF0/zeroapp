bfe989ad32e78d5156dab14bd4f52cd41ec9a369
Edoardo Fava <f.edoardo.0@gmail.com>
2022-11-21 21:22:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# zeroapp
+
+From zero programming knowledge to basic app development with Dart and Flutter using Android Studio.
---
+++ android/app/src/main/kotlin/com/edof0/zeroapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.edof0.zeroapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
