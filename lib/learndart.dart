6673eccb5ceac4ed26c0674f3d81e2fcf6b54609
Edoardo Fava <f.edoardo.0@gmail.com>
2022-11-29 11:51:29 +0100

    myMain doc

--- lib/learndart.dart
+++ lib/learndart.dart
@@ -1,6 +1,10 @@
 /// The text to print when the app starts, before pushing the button.
 const String initialMessage = "Push the button to run your function";
 
+/// Returns a string intended to be printed in the app.
+///
+/// This function is called everytime the app button is pressed,
+/// [counter] is the number of times the button was pressed.
 String? myMain(int counter) {
   return "Hello world!";
 }
